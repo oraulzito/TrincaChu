9063095d001487fd2b10fa9f0dc72539f9593d54
Raul Lacerda <raul_lacerda@outlook.com>
2021-10-26 00:09:24 -0300

    Forgotten migrations

---
+++ TrincaChu/Migrations/20211026015559_WhereItWillHappenOnEvents.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrincaChu.Migrations
+{
+    public partial class WhereItWillHappenOnEvents : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WhereItWillHappen",
+                table: "Event",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WhereItWillHappen",
+                table: "Event");
+        }
+    }
+}
